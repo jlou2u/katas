bbdeef5f2bf3c26d9764c173793724df3f01341f
Justin Lewis <justin.lewis@gmail.com>
2021-09-10 10:07:35 -0400

    problem 27

--- p99/python3/p27.py
+++ p99/python3/p27.py
@@ -1,9 +1,36 @@
 
-# group the elements of a set into disjoint subsets
+import itertools
 
-def group3(l):
-    pass
+def group(n, l, acc=[]):
+    if len(n) == 0:
+        return acc
+    results = []
+    for item in list(itertools.combinations(l, n[0])):
+        rem = [z for z in l if z not in item]
+        result = group(n[1:], rem, acc + [list(item)])
+        results.append(result)
+    if len(n) == 1:
+        return results[0]
+    else:
+        return results
+
+def test_group2():
+    result = group([1, 2], ['A', 'B', 'C'])
+    expected = [
+        [['A'], ['B', 'C']],
+        [['B'], ['A', 'C']],
+        [['C'], ['A', 'B']]
+    ]
+    assert expected == result
 
 
-def group(sizes, l):
-    pass
+def test_group3():
+    result = group([1, 1, 1], ['A', 'B', 'C'])
+
+    expected = [
+        [[['A'], ['B'], ['C']], [['A'], ['C'], ['B']]],
+        [[['B'], ['A'], ['C']], [['B'], ['C'], ['A']]],
+        [[['C'], ['A'], ['B']], [['C'], ['B'], ['A']]]
+    ]
+
+    assert expected == result
